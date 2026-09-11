061ef8f1ce335d0fd1bcafa32381c7693b08b5a7
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-11 09:48:41 +0700

    Menambahkan Jobsheet 1

---
+++ Jobsheet1/RoadBike26.java
@@ -0,0 +1,18 @@
+package Jobsheet1;
+
+public class RoadBike26 extends Bike26{
+
+    private int tireWidth;
+
+    public void setTireWidth(int width) {
+        tireWidth = width;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Tire Width: " + tireWidth + "mm");
+        System.out.println("Bike Type: Road Bike");
+    }
+    
+}
