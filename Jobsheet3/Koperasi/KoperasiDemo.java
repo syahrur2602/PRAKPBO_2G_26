bc995375f8b3ec96e32d1d5e6fa2d859a4f81d6e
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-12 13:28:49 +0700

    Percobaan 4 - Konstruktor, Instansiasi

--- Jobsheet3/Koperasi/KoperasiDemo.java
+++ Jobsheet3/Koperasi/KoperasiDemo.java
@@ -3,7 +3,9 @@ package Jobsheet3.Koperasi;
 public class KoperasiDemo {
 
     public static void main(String[] args) {
-        Anggota anggota1 = new Anggota();
+        Anggota anggota1 = new Anggota("Iwan", "Jalan Mawar");
+        System.out.println("Simpanan " + anggota1.getNama() + " : Rp " + anggota1.getSimpanan());
+
         anggota1.setNama("Iwan Setiawan");
         anggota1.setAlamat("Jalan Sukarno Hatta no 10");
         anggota1.setor(100000);
