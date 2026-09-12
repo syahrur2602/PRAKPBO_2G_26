3d901df172e08f9e4b058e33ff9329b6d9c02313
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-12 07:35:56 +0700

    Menambahkan Jobsheet 3

---
+++ Jobsheet3/motorEncapsulation/Motor.java
@@ -0,0 +1,15 @@
+package Jobsheet3.motorEncapsulation;
+
+public class Motor {
+    public int kecepatan = 0;
+    public boolean kontakOn = false;
+
+    public void printStatus() {
+        if (kontakOn == true) {
+            System.out.println("Kontak On");
+        } else {
+            System.out.println("Kontak Off");
+        }
+        System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+}
---
+++ Jobsheet3/motorEncapsulation/MotorDemo.java
@@ -0,0 +1,14 @@
+package Jobsheet3.motorEncapsulation;
+
+public class MotorDemo {
+
+    public static void main(String[] args) {
+        
+        Motor motor = new Motor();
+        motor.printStatus();
+        motor.kecepatan = 50;
+        motor.printStatus();
+
+    }
+    
+}
