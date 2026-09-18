b251ef5a4c79fd317b02a38317aa5da8220a29a3
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-18 08:42:14 +0700

    Jobsheet4_Percobaan3_26

---
+++ Jobsheet4/percobaan3/MainPertanyaan.java
@@ -0,0 +1,12 @@
+package Jobsheet4.percobaan3;
+
+public class MainPertanyaan {
+
+    public static void main(String[] args) {
+        Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
+        KeretaApi keretaApi =  new KeretaApi("Gaya Baru", "Bisnis", masinis);
+
+        System.out.println(keretaApi.info());
+    }
+    
+}
