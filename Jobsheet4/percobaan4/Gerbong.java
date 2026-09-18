bf8e2aace1625c19a9207e5b72077246d4bdda41
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-18 09:46:16 +0700

    Jobsheet4_Percobaan4_PertanyaanNo5_26

--- Jobsheet4/percobaan4/Gerbong.java
+++ Jobsheet4/percobaan4/Gerbong.java
@@ -18,7 +18,12 @@ public class Gerbong {
     }
 
     public void setPenumpang(Penumpang penumpang, int nomor) {
-        this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        if (this.arrayKursi[nomor - 1].getPenumpang() != null) {
+            System.out.println("Peringatan: Kursi nomor " + nomor + " sudah diisi oleh penumpang lain (" 
+                    + this.arrayKursi[nomor - 1].getPenumpang().getNama() + ")!");
+        } else {
+            this.arrayKursi[nomor - 1].setPenumpang(penumpang);
+        }
     }
 
     public String info() {
