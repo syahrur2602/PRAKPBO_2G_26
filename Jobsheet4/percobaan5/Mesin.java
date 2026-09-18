31533c9284b96bc8caf2a274663510c599a8d859
syahrurRAMADHAN2602 <flash24576@gmail.com>
2026-09-18 09:58:49 +0700

    Jobsheet4_Percobaan5_26

---
+++ Jobsheet4/percobaan5/Mesin.java
@@ -0,0 +1,15 @@
+package Jobsheet4.percobaan5;
+
+public class Mesin {
+
+    private String tipe;
+
+    public Mesin() {
+        this.tipe = "4-silinder";
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+    
+}
---
+++ Jobsheet4/percobaan5/Mobil.java
@@ -0,0 +1,18 @@
+package Jobsheet4.percobaan5;
+
+public class Mobil {
+
+    private String merek;
+    private Mesin mesin;
+
+    public Mobil(String merek) {
+        this.merek = merek;
+        this.mesin = new Mesin();
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Mobil: " + merek);
+        System.out.println("Mesin: " + mesin.getTipe());
+    }
+    
+}
